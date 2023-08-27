3ab7ddc2521ead5e3e72b2011d2ea028a48e0e5b
Vid Furlan <nalrufdiv@gmail.com>
2023-08-27 20:31:29 +0200

    No more symbolic links. Now actual files

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -1,1 +1,14 @@
-/home/vid/.config/polybar/launch.sh
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+# If all your bars have ipc enabled, you can use 
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar
+
+# Launch bar1 and bar2
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar mainbar 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar sidebar 2>&1 | tee -a /tmp/polybar2.log & disown
+
+echo "Bars launched..."
